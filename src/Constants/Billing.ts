fd14e86f26b4c6ef05e95f7fa89da88ef40f3b00
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2023-11-06 16:56:00 -0300

    Updated Subscription & Billing constants for shifts

--- src/Constants/Billing.ts
+++ src/Constants/Billing.ts
@@ -1,3 +1,4 @@
+// Citicen Contact
 export const MONTHLY_FEE_ENGAGE: number = 4.99;
 export const MONTHLY_FEE_ANALYTICS: number = 9.99;
 export const ANNUALLY_FEE_ENGAGE: number = 49.99;
@@ -5,3 +6,8 @@ export const ANNUALLY_FEE_ANALYTICS: number = 99.99;
 
 export const MONTHLY_FEE_ANALYTICS_COLORADO: number = 4.99;
 export const ANNUALLY_FEE_ANALYTICS_COLORADO: number = 49.99;
+
+// Shifts
+export const MONTHLY_FEE_SCHEDULE: number = 124;
+export const ANNUALLY_FEE_SCHEDULE: number = 12.4;
+// TODO CHANGE this
