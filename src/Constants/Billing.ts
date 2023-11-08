79fcfd8e3a914fe7a9541f75cbba859c8fd71942
Martín Weingart <martinweingart@gmail.com>
2023-11-08 08:05:49 -0300

    Update Billing.ts (#9)

--- src/Constants/Billing.ts
+++ src/Constants/Billing.ts
@@ -1,4 +1,4 @@
-// Citicen Contact
+// Citizen Contact
 export const MONTHLY_FEE_ENGAGE: number = 4.99;
 export const MONTHLY_FEE_ANALYTICS: number = 9.99;
 export const ANNUALLY_FEE_ENGAGE: number = 49.99;
@@ -8,6 +8,6 @@ export const MONTHLY_FEE_ANALYTICS_COLORADO: number = 4.99;
 export const ANNUALLY_FEE_ANALYTICS_COLORADO: number = 49.99;
 
 // Shifts
-export const MONTHLY_FEE_SCHEDULE: number = 124;
-export const ANNUALLY_FEE_SCHEDULE: number = 12.4;
+export const MONTHLY_FEE_SCHEDULE: number = 12.4;
+export const ANNUALLY_FEE_SCHEDULE: number = 124;
 // TODO CHANGE this
