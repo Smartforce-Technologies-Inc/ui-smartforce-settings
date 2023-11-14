b1977a1b9bca072585d87b0b730a1d6d4cf07d17
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2023-11-14 09:27:51 -0300

    Updated SectionMenu & SectionCard components for disabled state

--- src/Components/SectionMenu/SectionMenu.tsx
+++ src/Components/SectionMenu/SectionMenu.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import styles from './SectionMenu.module.scss';
+import { SFChip } from 'sfui';
 
 export interface SectionMenuProps {
   title: string;
   selected: boolean;
+  disabled?: boolean;
   onClick: () => void;
 }
 
 const SectionMenu = ({
   title,
   selected,
+  disabled,
   onClick
 }: SectionMenuProps): React.ReactElement<SectionMenuProps> => {
   return (
     <p
-      className={`${styles.sectionMenu} ${selected ? styles.selected : ''}`}
-      onClick={onClick}
+      className={`${styles.sectionMenu} ${selected ? styles.selected : ''} ${
+        disabled ? styles.disabled : ''
+      }`}
+      onClick={!disabled ? onClick : undefined}
     >
       {title}
+      {disabled && (
+        <SFChip size="small" sfColor="default" label="Coming Soon" />
+      )}
     </p>
   );
 };
