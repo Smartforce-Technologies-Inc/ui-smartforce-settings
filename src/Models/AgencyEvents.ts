d3189f78e9466b0d485aa50ac8b628a0f3751dc5
Yopo Nicolas <nicolas.yopo@smartforcetech.com>
2023-11-16 16:13:53 -0300

    Added AgencyEventsType response modal

--- src/Models/AgencyEvents.ts
+++ src/Models/AgencyEvents.ts
@@ -2,3 +2,14 @@ export interface AgencyEvent {
   name: string;
   color: string;
 }
+
+export interface AgencyEventTypeResponse {
+  id: number;
+  agency_id: number;
+  name: string;
+  color: string;
+  created_at: string;
+  updated_at: string;
+  created_by_user: string;
+  updated_by_user: string;
+}
