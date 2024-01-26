9cb19b51c4234b85b89b69aa26ba92d73e7ea176
Martin Weingart <martinweingart@gmail.com>
2024-01-26 12:38:06 -0300

    Add forwardRef to GoogleMap component

--- src/Components/GoogleMap/GoogleMap.tsx
+++ src/Components/GoogleMap/GoogleMap.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, memo } from 'react';
+import React, { Fragment, forwardRef, memo } from 'react';
 import styles from './GoogleMap.module.scss';
 import {
   defaultMapOptions,
@@ -26,92 +26,95 @@ export interface GoogleMapProps extends google.maps.MapOptions {
 
 // Using React.memo to avoid unnecessary re renders
 export const GoogleMap = memo(
-  ({
-    bounds,
-    center,
-    zoom,
-    children,
-    onCreate,
-    onClick,
-    onCenterChange
-  }: GoogleMapProps): React.ReactElement<GoogleMapProps> => {
-    const { themeType } = React.useContext(ThemeTypeContext);
-    const mapStyles = themeType === 'night' ? nightModeStyle : defaultMapStyle;
-    const [map, setMap] = React.useState<google.maps.Map>();
-    const refContainer = React.useRef<HTMLDivElement>(null);
+  forwardRef<HTMLDivElement, GoogleMapProps>(
+    (
+      { bounds, center, zoom, children, onCreate, onClick, onCenterChange },
+      ref
+    ) => {
+      const { themeType } = React.useContext(ThemeTypeContext);
+      const mapStyles =
+        themeType === 'night' ? nightModeStyle : defaultMapStyle;
+      const [map, setMap] = React.useState<google.maps.Map>();
+      const refContainer = React.useRef<HTMLDivElement>(null);
 
-    React.useEffect(() => {
-      if (refContainer.current && !map) {
-        const map = new window.google.maps.Map(refContainer.current, {});
-        setMap(map);
-        addSmartforceLegend(refContainer);
-        onCreate && onCreate(map);
-      }
-    }, [map, onCreate]);
+      React.useImperativeHandle<HTMLDivElement | null, HTMLDivElement | null>(
+        ref,
+        () => refContainer.current
+      );
 
-    React.useEffect(() => {
-      if (map) {
-        map.setOptions({
-          styles: mapStyles,
-          ...defaultMapOptions,
-          center,
-          zoom
-        });
-      }
-    }, [map, mapStyles, center, zoom]);
-
-    React.useEffect(() => {
-      if (map) {
-        google.maps.event.clearListeners(map, 'click');
-
-        if (onClick) {
-          map.addListener('click', onClick);
+      React.useEffect(() => {
+        if (refContainer.current && !map) {
+          const map = new window.google.maps.Map(refContainer.current, {});
+          setMap(map);
+          addSmartforceLegend(refContainer);
+          onCreate && onCreate(map);
         }
-      }
-    }, [map, onClick]);
+      }, [map, onCreate]);
 
-    React.useEffect(() => {
-      if (map) {
-        google.maps.event.clearListeners(map, 'center_changed');
-
-        if (onCenterChange) {
-          map.addListener('center_changed', () =>
-            onCenterChange(map.getCenter()?.toJSON())
-          );
+      React.useEffect(() => {
+        if (map) {
+          map.setOptions({
+            styles: mapStyles,
+            ...defaultMapOptions,
+            center,
+            zoom
+          });
         }
-      }
-    }, [map, onCenterChange]);
+      }, [map, mapStyles, center, zoom]);
 
-    React.useEffect(() => {
-      if (map && bounds) {
-        map.fitBounds(bounds);
-      }
-    }, [map, bounds]);
+      React.useEffect(() => {
+        if (map) {
+          google.maps.event.clearListeners(map, 'click');
 
-    // Props to avoid dragging when user is panning
-    const dragProps = {
-      draggable: true,
-      onDragStart: (e: React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        e.stopPropagation();
-      }
-    };
+          if (onClick) {
+            map.addListener('click', onClick);
+          }
+        }
+      }, [map, onClick]);
 
-    return (
-      <Fragment>
-        <div
-          ref={refContainer}
-          className={styles.googleMap}
-          {...dragProps}
-        ></div>
+      React.useEffect(() => {
+        if (map) {
+          google.maps.event.clearListeners(map, 'center_changed');
 
-        {children &&
-          React.Children.map(children, (child) => {
-            if (React.isValidElement<{ map: google.maps.Map }>(child)) {
-              return React.cloneElement(child, { map });
-            } else return null;
-          })}
-      </Fragment>
-    );
-  }
+          if (onCenterChange) {
+            map.addListener('center_changed', () =>
+              onCenterChange(map.getCenter()?.toJSON())
+            );
+          }
+        }
+      }, [map, onCenterChange]);
+
+      React.useEffect(() => {
+        if (map && bounds) {
+          map.fitBounds(bounds);
+        }
+      }, [map, bounds]);
+
+      // Props to avoid dragging when user is panning
+      const dragProps = {
+        draggable: true,
+        onDragStart: (e: React.DragEvent<HTMLDivElement>) => {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      };
+
+      return (
+        <Fragment>
+          <div
+            ref={refContainer}
+            className={styles.googleMap}
+            {...dragProps}
+          ></div>
+
+          {children &&
+            React.Children.map(children, (child) => {
+              if (React.isValidElement<{ map: google.maps.Map }>(child)) {
+                return React.cloneElement(child, { map });
+              } else return null;
+            })}
+        </Fragment>
+      );
+    }
+  )
 );
